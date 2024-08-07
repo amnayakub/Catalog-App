b9dcb0be00f7eb497122a0d2f83fae41a65576c1
Amna Khan <amnayaqoob909@gmail.com>
2024-08-07 23:25:47 +0500

    commited asf

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,23 +1,99 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
-  const MyDrawer
-({super.key});
+  const MyDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const imageURL = "https://icons.veryicon.com/png/o/internet--web/prejudice/user-128.png";
     return Drawer(
-      child: ListView(
-        children: const [
-          DrawerHeader(
-            padding: EdgeInsets.zero,
-            child: UserAccountsDrawerHeader(
+      child: Container(
+        color: const Color(0xFF161514),
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
               margin: EdgeInsets.zero,
-              accountName: Text("Amna Yakub"),
-              accountEmail: Text("amnayaqoob909@gmail.com"),
-            )
-          )
-        ],
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [Color.fromARGB(255, 71, 70, 70), Color(0xFF161514)],
+                  begin: Alignment.bottomLeft,
+                  end: Alignment.topRight,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    spreadRadius: 3,
+                    blurRadius: 5,
+                    offset: const Offset(0, 3),
+                  ),
+                ],
+              ),
+              currentAccountPicture: const CircleAvatar(
+                backgroundImage: NetworkImage(imageURL),
+              ),
+              accountName: const Text(
+                "User",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 25,
+                  color: Colors.white,
+                ),
+              ),
+              accountEmail: const Text(
+                "user111@gmail.com",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                textScaleFactor: 1.2,
+                style: TextStyle(
+                  color: Colors.white,
+                  //fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
